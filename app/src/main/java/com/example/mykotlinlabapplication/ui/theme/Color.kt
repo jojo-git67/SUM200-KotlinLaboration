df0052218e2b4015a3c0e14c55b6935fc6990bab
jojo-git67 <joakim.johansson.16@student.hv.se>
2026-09-18 13:33:18 +0200

    Updated default theme

--- app/src/main/java/com/example/mykotlinlabapplication/ui/theme/Color.kt
+++ app/src/main/java/com/example/mykotlinlabapplication/ui/theme/Color.kt
@@ -2,10 +2,8 @@ package com.example.mykotlinlabapplication.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val SkyBlue = Color(0xFF4A90B8)
+val SkyBlueLight = Color(0xFFB9DDF0)
+val CloudWhite = Color(0xFFF7FAFC)
+val DarkBlue = Color(0xFF193B4D)
+val SunnyYellow = Color(0xFFFFC857)
