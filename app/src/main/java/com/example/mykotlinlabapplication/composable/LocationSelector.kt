be792f31fe6d1c088481854998b472ad77942f1c
jojo-git67 <joakim.johansson.16@student.hv.se>
2026-09-20 14:53:23 +0200

    Updated deprecated modifier.menuAnchor()

--- app/src/main/java/com/example/mykotlinlabapplication/composable/LocationSelector.kt
+++ app/src/main/java/com/example/mykotlinlabapplication/composable/LocationSelector.kt
@@ -1,8 +1,10 @@
 package com.example.mykotlinlabapplication.composable
 
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuAnchorType
 import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.Text
@@ -11,6 +13,7 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.example.mykotlinlabapplication.data.Location
+
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -43,7 +46,11 @@ fun LocationSelector(
                     expanded = expanded
                 )
             },
-            modifier = Modifier.menuAnchor()
+            modifier = Modifier
+                .menuAnchor(
+                type = ExposedDropdownMenuAnchorType.PrimaryNotEditable
+            )
+                .fillMaxWidth()
         )
 
         ExposedDropdownMenu(
